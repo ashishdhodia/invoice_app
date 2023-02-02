ef9ece26d1a11f6fe058c06234f94f2c1e1d554d
ashishdhodia <ashishdhodia@gmail.com>
2023-02-02 20:18:52 +0530

    Added necessary components and routing

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from '@angular/core'
+import { RouterModule, Routes } from '@angular/router'
+import { NavbarComponent } from './navbar/navbar.component'
+import { InvoicesComponent } from './invoices/invoices.component'
+import { ClientsComponent } from './clients/clients.component'
+import { ItemsComponent } from './items/items.component'
+import { LoginComponent } from './login/login.component'
+import { ProfileComponent } from './profile/profile.component'
+import { HomeComponent } from './home/home.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "navbar", component: NavbarComponent },
+  { path: "home", component: HomeComponent },
+  { path: "invoices", component: InvoicesComponent },
+  { path: "clients", component: ClientsComponent },
+  { path: "items", component: ItemsComponent },
+  { path: "profile", component: ProfileComponent },
+  { path: "login", component: LoginComponent }
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,10 +5,24 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GraphQLModule } from './graphql.module';
 import { HttpClientModule } from '@angular/common/http';
+import { NavbarComponent } from './navbar/navbar.component';
+import { InvoicesComponent } from './invoices/invoices.component';
+import { ClientsComponent } from './clients/clients.component';
+import { ItemsComponent } from './items/items.component';
+import { LoginComponent } from './login/login.component';
+import { ProfileComponent } from './profile/profile.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    InvoicesComponent,
+    ClientsComponent,
+    ItemsComponent,
+    LoginComponent,
+    ProfileComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
